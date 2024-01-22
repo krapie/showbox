26d35d784190552b7fb4d429dba10ab326c1df0f
Kevin Park <krapi0314@gmail.com>
2024-01-22 16:20:48 +0900

    Scaffold CLI

---
+++ cmd/server.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2024 Kevin Park <krapi0314@gmail.com>
+*/
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// serverCmd represents the server command
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "Runs showbox API server for show ticketing.",
+	Long:  `Runs showbox API server for show ticketing.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("server called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serverCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// serverCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// serverCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 Kevin Park <krapi0314@gmail.com>
+*/
+
+package main
+
+import "github.com/krapie/showbox/cmd"
+
+func main() {
+	cmd.Execute()
+}
